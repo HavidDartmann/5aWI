07db16b896985b1452dfaa20f4bea70a9e2ba186
HavidDartmann <david.hartmann@student.htldornbirn.at>
2022-10-18 15:15:51 +0200

    Commit

--- src/cars11/Car.java
+++ src/cars11/Car.java
@@ -4,18 +4,58 @@ public class Car {
     private String color;
     private int maxSpeed;
     private int basePrice;
-    private int baseConsumption;
+    private float baseConsumption;
     private Engine engine;
     private Producer producer;
+    private float reducedPrice;
+    private int mileage;
 
-    public Car(String color, int maxSpeed, int basePrice, int baseConsumption, Engine engine, Producer producer){
+    public Car(String color, int maxSpeed, int basePrice, float baseConsumption, Engine engine, Producer producer){
         this.color = color;
         this.maxSpeed = maxSpeed;
         this.basePrice = basePrice;
         this.baseConsumption = baseConsumption;
         this.engine = engine;
         this.producer = producer;
+        this.reducedPrice = (float) (basePrice - (basePrice * producer.getDiscount()));
     }
 
+    public int getMileage() {
+        return mileage;
+    }
 
+    public void setMileage(int mileage) {
+        if (mileage > 50000) {
+            baseConsumption += (float) (baseConsumption * 0.098f);
+        }
+        this.mileage = mileage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public float getBaseConsumption() {
+        return baseConsumption;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public float getReducedPrice() {
+        return reducedPrice;
+    }
 }
--- src/cars11/Main.java
+++ src/cars11/Main.java
@@ -1,7 +1,16 @@
 package cars11;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args){
+        Car c1 = new Car("red", 200, 10000, 10f, new Engine(Engine.Type.DIESEL, 100), new Producer("Fiat", "Italy", 0.1f));
 
+        System.out.println(c1.getBasePrice());
+        System.out.println(c1.getReducedPrice());
+
+        System.out.println(c1.getBaseConsumption());
+        c1.setMileage(60000);
+        System.out.println(c1.getBaseConsumption());
     }
 }
